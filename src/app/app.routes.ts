c23734d09f92a3a03e48e5a6430d63838564bad0
LucasAdrian0 <https://github.com/LucasAdrian0>
2024-10-22 18:09:33 -0300

    breve tentativa de correções das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
+//arquivo para estruturar as rotas//
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { Component, NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+
+const routes: Routes = [
+    {path:'', component: AppComponent}
+    
+];
+
+@NgModule({
+    declarations:[],
+    imports:[
+        RouterModule.forRoot(routes),
+        Component 
+    ],
+    exports:[RouterModule],
+})
+export class AppRoutingModule{}
